754cb47786ffa20583cb3bce0246df968fe1540d
MoedCode <mohamed71291@gmail.com>
2024-02-03 18:36:56 +0200

    dir api

--- pre_project/pytest.py
+++ pre_project/pytest.py
@@ -3,11 +3,11 @@
 from flask import Flask, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://procode:moed22@localhost/test_flaskr'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+APP = Flask(__name__)
+APP.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://procode:moed22@localhost/test_flaskr'
+APP.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+db = SQLAlchemy(APP)
 
 # Define your model
 class Entry(db.Model):
@@ -20,21 +20,21 @@ def init_db():
     db.create_all()
 
 # Teardown Request: Close Database Connection
-@app.teardown_appcontext
+@APP.teardown_appcontext
 def close_db(error):
     db.session.remove()
 
 # Route to fetch entries as JSON
-@app.route('/api/entries', methods=['GET'])
+@APP.route('/api/entries', methods=['GET'])
 def get_entries():
     entries = Entry.query.all()
     entries_list = [{'id': entry.id, 'title': entry.title, 'text': entry.text} for entry in entries]
     return jsonify(entries_list)
 
 # Route to serve HTML page with a table
-@app.route('/')
+@APP.route('/')
 def show_entries():
     return render_template('pytst.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    APP.run(host='0.0.0.0', port=5000)
