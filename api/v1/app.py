689a4b4826ddb668b2835a0df9bf67c6053da1df
MoedCode <mohamed71291@gmail.com>
2024-02-04 05:39:47 +0200

    api: app.py index.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+from flask import Flask
+
+from models import storage
+from api.v1.views import app_view
+import os
+
+APP = Flask(__name__)
+
+APP.register_blueprint(app_view, url_prefix='/api/v')
+@APP.teardown_appcontext
+def teardown(exception):
+    "CClose storage"
+    storage.close()
+if __name__ == "__main__":
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.getenv('HBNB_API_PORT', 5000))
+    APP.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+from api.v1.views import app_view
+from flask import jsonify
+
+@app_view.route('/status', methods=['GET'])
+def api_status():
+    "Routs API status"
+    return jsonify({"status":"ok"})
