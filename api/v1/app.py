00f6f7c21d8cd757815d4e2978b8fe22fca2470a
MoedCode <mohamed71291@gmail.com>
2024-02-04 17:31:33 +0200

    api: app.py index.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,18 +1,36 @@
 #!/usr/bin/python3
-from flask import Flask
+"""this is flask app"""
 
+from flask import Flask, jsonify
 from models import storage
-from api.v1.views import app_view
-import os
+from os import getenv
+from api.v1.views import app_views
+from flask_cors import CORS
 
-APP = Flask(__name__)
+app = Flask(__name__)
+app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+app.url_map.strict_slashes = False
 
-APP.register_blueprint(app_view, url_prefix='/api/v')
-@APP.teardown_appcontext
-def teardown(exception):
-    "CClose storage"
+
+@app.teardown_appcontext
+def close(self):
+    """close the session"""
     storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    '''return render_template'''
+    data = {'error': 'Not found'}
+    return jsonify(data), 404
+
+
 if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', 5000))
-    APP.run(host=host, port=port, threaded=True)
+    host = getenv('HBNB_API_HOST')
+    port = getenv('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
+    app.run(host=host, port=port, threaded=True)
