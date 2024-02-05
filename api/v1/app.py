27ab2ce4ed1c69d053a085677427428771715819
MoedCode <mohamed71291@gmail.com>
2024-02-05 23:22:59 +0200

    6 .  app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""this is flask app"""
+"""this is flask APP"""
 
 from flask import Flask, jsonify
 from models import storage
@@ -7,19 +7,19 @@ from os import getenv
 from api.v1.views import app_views
 from flask_cors import CORS
 
-app = Flask(__name__)
-app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-app.url_map.strict_slashes = False
+APP = Flask(__name__)
+APP.register_blueprint(app_views)
+cors = CORS(APP, resources={r"/api/v1/*": {"origins": "*"}})
+APP.url_map.strict_slashes = False
 
 
-@app.teardown_appcontext
+@APP.teardown_appcontext
 def close(self):
     """close the session"""
     storage.close()
 
 
-@app.errorhandler(404)
+@APP.errorhandler(404)
 def page_not_found(error):
     '''return render_template'''
     data = {'error': 'Not found'}
@@ -33,4 +33,4 @@ if __name__ == "__main__":
         host = '0.0.0.0'
     if not port:
         port = '5000'
-    app.run(host=host, port=port, threaded=True)
+    APP.run(host=host, port=port, threaded=True)
