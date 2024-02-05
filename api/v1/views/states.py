d5ef57d7d3ec3fa27d9c1d3959e224e73a6294d2
MoedCode <mohamed71291@gmail.com>
2024-02-05 03:35:23 +0200

    6 . GET/DELETE /

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 from flask import jsonify
-from models.state import State, abort
+from models.state import State, abort, request
 from models import storage
 from api.v1.views import app_views
+
+# Retrieves the list of all /Retrieves a
 
 
 @app_views.route('/states/', methods=['GET'])
@@ -23,3 +24,18 @@ def getState_byId(state_id):
     if not state_inst:
         abort(404, 'Not found')
     return jsonify(state_inst.to_dict()), 200
+
+    # DELETE/
+
+
+@app_views.route('/states/<string:state_id>/', methods=['DELETE'])
+def del_state(state_id):
+    """Deletes A specified state by id"""
+    state_obj = storage.get('State', state_id)
+    if state_obj is None:
+        abort(404)
+    storage.delete(state_obj)
+    storage.save()
+    return jsonify({}), 200
+
+# Creates a
