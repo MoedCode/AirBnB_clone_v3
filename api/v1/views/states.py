8150402c6334f4137128f1ac785e6a5086f4ffac
MoedCode <mohamed71291@gmail.com>
2024-02-05 23:19:59 +0200

    6 .  DELET

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -8,63 +8,51 @@ from models.state import State
 
 
 @app_views.route('/states/', methods=['GET'])
-def listofstatus():
-    """comment for func"""
-    ob = storage.all('State')
-    ll = []
-    for state in ob.values():
-        ll.append(state.to_dict())
-    return jsonify(ll)
+def get_states():
+    """cash all state objects from storage"""
+    states_obj = storage.all('State')
+    states_lis = []
+    for state in states_obj.values():
+        states_lis.append(state.to_dict())
+    return jsonify(states_lis)
 
 
 @app_views.route('/states/<string:state_id>/', methods=['GET'])
-def StatusWithId(state_id):
-    """gets obj with id"""
-    ob = storage.get(State, state_id)
-    if not ob:
+def getState_byId(state_id):
+    """rout state data for specified id"""
+    state_inst = storage.get(State, state_id)
+    if not state_inst:
         abort(404, 'Not found')
-    return jsonify(ob.to_dict()), 200
+    return jsonify(state_inst.to_dict()), 200
 
 
 @app_views.route('/states/<string:state_id>/', methods=['DELETE'])
-def DeleteObj(state_id):
-    """deletes obj"""
-    x = storage.get('State', state_id)
-    if x is None:
+def del_state(state_id):
+    """Deletes A specified state by id"""
+    state_obj = storage.get('State', state_id)
+    if state_obj is None:
         abort(404)
-    storage.delete(x)
+    storage.delete(state_obj)
     storage.save()
     return jsonify({}), 200
 
 
-@app_views.route('/states/<string:state_id>/', methods=['PUT'])
-def putstate(state_id):
-    """put state"""
-    response = request.get_json()
-    if not response:
-        abort(400, {'Not a JSON'})
-    if response.get('name') is None:
-        abort(400, {'Missing name'})
-    stateObject = storage.get('State', state_id)
-    if stateObject is None:
-        abort(404)
-    ignoreKeys = ['id', 'created_at', 'updated_at']
-    for key, val in response.items():
-        if key not in ignoreKeys:
-            setattr(stateObject, key, val)
-    storage.save()
-    return jsonify(stateObject.to_dict()), '200'
-
-
 @app_views.route('/states/', methods=['POST'])
-def poststate():
-    """post state"""
-    response = request.get_json()
-    if not response:
+def post_state():
+    """obtaining a state data instantiate state """
+    # receive a post request
+    state_JData = request.get_json()
+    # vallation checks
+    if not state_JData:
         abort(400, {'Not a JSON'})
-    if not response.get('name'):
+    if not state_JData.get('name'):
         abort(400, {'Missing name'})
-    stateObject = State(**response)
-    storage.new(stateObject)
+
+    # insatiate instant with request data
+    state_inst = State(**state_JData)
+
+    # update storage type with state data and save
+    storage.new(state_inst)
     storage.save()
-    return jsonify(stateObject.to_dict()), '201'
+    # 201 indicates that the saving process is done
+    return jsonify(state_inst.to_dict()), '201'
