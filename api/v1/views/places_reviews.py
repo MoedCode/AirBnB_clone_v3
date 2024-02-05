8aba2a8556ddbbc6f1daf799de07d12b56cbb86f
Nasef161995~ <mnasef905@gmail.com>
2024-02-05 23:14:09 +0300

    10

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python3
 """Review object that handles all default RESTFul API actions"""
 
+from models.review import Review
 from api.v1.views import app_views
 from models import storage
 from models.place import Place
 from models.user import User
-from models.review import Review
 from flask import abort, request, jsonify
 
 @app_views.route("/places/<place_id>/reviews")
 def get_reviews(place_id):
-    """ return reviews"""
+    """return reviews"""
     place = storage.get(Place, place_id)
     if not place:
         abort(404)
     return jsonify([review.to_dict() for review in place.reviews])
+
+
+@app_views.route("/reviews/<review_id>")
+def get_review(review_id):
+    """return review"""
+    review = storage.get(Review, review_id)
+    if not review:
+        abort(404)
+    return jsonify(review.to_dict())
