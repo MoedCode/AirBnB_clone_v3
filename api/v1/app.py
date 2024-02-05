eded0ecfa0d8e9208867e8785bb2612b9c22ccfb
MoedCode <mohamed71291@gmail.com>
2024-02-05 05:03:45 +0200

    6 . GET/DELETE /

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,6 +12,9 @@ APP.register_blueprint(app_views)
 cors = CORS(APP, resources={r"/api/v1/*": {"origins": "*"}})
 APP.url_map.strict_slashes = False
 
+HOST = getenv('HBNB_API_HOST')
+PORT = getenv('HBNB_API_PORT')
+
 
 @APP.teardown_appcontext
 def close(self):
@@ -27,10 +30,8 @@ def page_not_found(error):
 
 
 if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST')
-    port = getenv('HBNB_API_PORT')
-    if not host:
-        host = '0.0.0.0'
-    if not port:
-        port = '5000'
-    APP.run(host=host, port=port, threaded=True)
+    if not HOST:
+        HOST = '0.0.0.0'
+    if not PORT:
+        PORT = '5000'
+    APP.run(host=HOST, port=PORT, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -22,11 +22,11 @@ def statusok():
 def statssok():
     """stats home page"""
     data = {
-            "amenities": storage.count(Amenity),
-            "cities": storage.count(City),
-            "places": storage.count(Place),
-            "reviews": storage.count(Review),
-            "states": storage.count(State),
-            "users": storage.count(User),
-           }
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User),
+    }
     return jsonify(data)
