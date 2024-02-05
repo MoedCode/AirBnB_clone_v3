c921206ba569d1fa9dec0066d4bab435ec2339ab
MoedCode <mohamed71291@gmail.com>
2024-02-05 05:34:07 +0200

    6 . GET/DELETE /

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""the index file for stats"""
+"""status routs file"""
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
@@ -12,21 +12,21 @@ from models.user import User
 
 
 @app_views.route('status', strict_slashes=False)
-def statusok():
-    """status home page"""
+def home_statues():
+    """Home Page Status """
     status = {"status": "OK"}
     return jsonify(status), 200
 
 
 @app_views.route('stats', strict_slashes=False)
-def statssok():
-    """stats home page"""
+def home_api():
+    """BNB routs"""
     data = {
-            "amenities": storage.count(Amenity),
-            "cities": storage.count(City),
-            "places": storage.count(Place),
-            "reviews": storage.count(Review),
-            "states": storage.count(State),
-            "users": storage.count(User),
-           }
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User),
+    }
     return jsonify(data)
