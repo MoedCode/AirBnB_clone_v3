ee38e49ad9f03ab45bc9133542b33d0ae034132a
MoedCode <mohamed71291@gmail.com>
2024-02-06 05:06:49 +0200

    places 2 get (automated command)

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -14,18 +14,20 @@ from models.state import State
 @app_views.route('/cities/<string:city_id>/places', methods=['GET'],
                  strict_slashes=False)
 def get_places(city_id):
-    """Get place information for all places in a specified city."""
+    """Get place information for all places in a specified city"""
     city = storage.get(City, city_id)
     if city is None:
         abort(404)
-    places = [place.to_dict() for place in city.places]
+    places = []
+    for place in city.places:
+        places.append(place.to_dict())
     return jsonify(places)
 
 
 @app_views.route('/places/<string:place_id>', methods=['GET'],
                  strict_slashes=False)
 def get_place(place_id):
-    """Get place information for specified place."""
+    """Get place information for specified place"""
     place = storage.get(Place, place_id)
     if place is None:
         abort(404)
@@ -35,19 +37,19 @@ def get_place(place_id):
 @app_views.route('/places/<string:place_id>', methods=['DELETE'],
                  strict_slashes=False)
 def delete_place(place_id):
-    """Deletes a place based on its place_id."""
+    """Deletes a place based on its place_id"""
     place = storage.get(Place, place_id)
     if place is None:
         abort(404)
     place.delete()
     storage.save()
-    return jsonify({})
+    return (jsonify({}))
 
 
 @app_views.route('/cities/<string:city_id>/places', methods=['POST'],
                  strict_slashes=False)
 def post_place(city_id):
-    """Create a new place."""
+    """Create a new place"""
     city = storage.get(City, city_id)
     if city is None:
         abort(404)
@@ -62,15 +64,15 @@ def post_place(city_id):
     if 'name' not in kwargs:
         return make_response(jsonify({'error': 'Missing name'}), 400)
     kwargs['city_id'] = city_id
-    place = Place(**kwargs)
-    place.save()
-    return make_response(jsonify(place.to_dict()), 201)
+    new_place = Place(**kwargs)
+    new_place.save()
+    return make_response(jsonify(new_place.to_dict()), 201)
 
 
 @app_views.route('/places/<string:place_id>', methods=['PUT'],
                  strict_slashes=False)
 def put_place(place_id):
-    """Update a place."""
+    """Update a place"""
     place = storage.get(Place, place_id)
     if place is None:
         abort(404)
@@ -86,23 +88,24 @@ def put_place(place_id):
 
 @app_views.route('/places_search', methods=['POST'], strict_slashes=False)
 def post_places_search():
-    """Search for a place."""
+    """Searches for a place"""
     if request.get_json() is not None:
         params = request.get_json()
         states = params.get('states', [])
         cities = params.get('cities', [])
         amenities = params.get('amenities', [])
-        amenity_objects = [storage.get(Amenity, amenity_id) for amenity_id in amenities if storage.get(Amenity, amenity_id)]
+        amenity_objects = []
+        for amenity_id in amenities:
+            amenity = storage.get(Amenity, amenity_id)
+            if amenity:
+                amenity_objects.append(amenity)
         if states == cities == []:
             places = storage.all('Place').values()
         else:
-            places = [place for state_id in states
-                      for state in [storage.get(State, state_id)]
-                      for city_id in [city.id for city in state.cities]
-                      for city in [storage.get(City, city_id)]
-                      for place in city.places]
-        confirmed_places = [place.to_dict() for place in places
-                            if all(amenity in place.amenities for amenity in amenity_objects)]
-        return jsonify(confirmed_places)
-    else:
-        return make_response(jsonify({'error': 'Not a JSON'}), 400)
+            places = []
+            for state_id in states:
+                state = storage.get(State, state_id)
+                state_cities = state.cities
+                for city in state_cities:
+                    if city.id not in cities:
+                        cities.append(city.
